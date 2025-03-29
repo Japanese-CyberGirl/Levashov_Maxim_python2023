8496263dcbf45e01a8bd2f078f66c0118082f472
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-29 11:35:50 +0700

    little changes

--- hard sort.py
+++ hard sort.py
@@ -3,10 +3,10 @@ from collections import namedtuple
 def custom_sort(students_list):
     return sorted(students_list, key=lambda student: -(len(student.name) + len(student.surname)))
 
-# Создаем namedtuple для хранения данных о студентах
+
 Student = namedtuple('Student', ['name', 'surname'])
 
-# Создаем список студентов
+
 students = [
     Student(name='ghhjgyu', surname='guyybjug'),
     Student(name='hgy', surname='hgg'),
@@ -14,7 +14,6 @@ students = [
     Student(name='anna', surname='johnson')
 ]
 
-# Сортируем и выводим результат
 sorted_students = custom_sort(students)
 for student in sorted_students:
     print(f"Name: {student.name}, Surname: {student.surname}")
