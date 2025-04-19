656ebdcc65ec747bb229e7dddfea26db209fe16c
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-04-19 14:51:50 +0700

    it seems to be, that HW-01 part 1 is done

--- check_ignored.py
+++ check_ignored.py
@@ -10,23 +10,41 @@ def git_ignored_path(project_dir):
             line = line.strip() #избавляемся от всяких там \n
             if not line or line.startswith('#'): #скипаем комментарии и пустые строки
                 continue
+            if line[0] == '/':
+                line = line[1:]
             ignored_types.append(line)
         return ignored_types
 
 def ignored_regex(ignored_types):
-    regex_types = [[],[]]
+    regex_types = []
     for i in range(len(ignored_types)):
         element = ignored_types[i]
         if (element[0] == '*'):
-            regex_types[0].append(element)
-            regex_types[1].append(len(element))
+            regex_types.append(element)
     return regex_types
 
 parser = argparse.ArgumentParser(description="Directory ignored files")
 parser.add_argument("--project_dir", help="Path to the file", type = str, required = True)
 
 args = parser.parse_args()
-print(args.project_dir)
-print(list(os.listdir(args.project_dir)))
-print(git_ignored_path(args.project_dir))
+#print(args.project_dir)
+#print(list(os.listdir(args.project_dir)))
+#print(git_ignored_path(args.project_dir))
 ignored_regex(git_ignored_path(args.project_dir))
+
+check_array = git_ignored_path(args.project_dir)
+regex_check_array = ignored_regex(check_array)
+
+print("Ignored files")
+
+
+for root, dirs, files in os.walk(args.project_dir):
+    for file in files:
+        if file in check_array:
+            print(f"{os.path.join(root,file)} ignored by expression {file}")
+            continue
+
+        for pattern in regex_check_array:
+            if fnmatch.fnmatch(file, pattern):
+                print(f"{os.path.join(root,file)} ignored by expression {pattern}")
+                break
